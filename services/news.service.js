080c5f6d28b0f8bebefafe0a2a8a4f6966d5ce2e
realDarkCode <tanzil.ncpsc@gmail.com>
2022-10-04 22:52:28 +0600

    feat!: add local news fetch system

--- services/news.service.js
+++ services/news.service.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const error = require("../error/error");
 const fetch = require("node-fetch");
 
-const baseUrl = "https://newsapi.org/v2";
+const { parse } = require("node-html-parser");
+
 const fetchInternationalNews = async (sources = "bbc-news") => {
+  const baseUrl = "https://newsapi.org/v2";
   const response = await fetch(
     `${baseUrl}/top-headlines?sources=${sources}&apiKey=${process.env.NEWS_API_KEY}`
   );
@@ -12,22 +14,48 @@ const fetchInternationalNews = async (sources = "bbc-news") => {
 
   if (data?.status === "error") error(data?.code, data?.message);
 
-  return {
-    news: data?.articles,
-    totalNews: data?.totalResults,
-  };
-};
-
-const getInternationalNews = async () => {
-  const { news } = await fetchInternationalNews();
-
-  if (!news) error("No news found");
-  const compressedNews = (news || []).map((article) => ({
+  const compressedNews = (data?.articles || []).map((article) => ({
     title: article.title,
     publishedAt: article.publishedAt,
     url: article.url,
   }));
-
   return compressedNews;
 };
-module.exports = { getInternationalNews };
+
+const fetchLocalNews = async () => {
+  const URL = "https://www.prothomalo.com/collection/latest";
+  const SELECTOR =
+    "#container > div > div.two-stories-with-load-more-wrapper.stories2AdWithLoadMore-m__wrapper__2Nvc4 > div.stories-set.stories2AdWithLoadMore-m__stories_set__27iKs > div > a";
+  const news = [];
+
+  const response = await fetch(URL);
+  const body = await response.text();
+  const document = parse(body);
+  const content = document.querySelectorAll(SELECTOR);
+  content.forEach((child) => {
+    if (!child) return;
+    news.push({
+      title: child.getAttribute("aria-label"),
+      url: child.getAttribute("href"),
+    });
+  });
+
+  return news;
+};
+
+const getNews = async (options) => {
+  const opt = {
+    type: "IN",
+    length: 10,
+    ...options,
+  };
+  let news = [];
+  if (opt.type === "IN") {
+    news = await fetchInternationalNews();
+  } else {
+    news = await fetchLocalNews();
+  }
+
+  return news.slice(0, 10);
+};
+module.exports = { getNews };
