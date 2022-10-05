6f8dfd01891939291519bcffaf78256b785d9fb2
realDarkCode <tanzil.ncpsc@gmail.com>
2022-10-05 20:45:06 +0600

    feat: add rate limit

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ require("dotenv").config("../.env");
 const express = require("express");
 const { notFoundHandler, errorHandler } = require("./error");
 const app = express();
+
+// settings
+app.set("trust proxy", 1);
+// middleware and routes
 app.use(require("./middleware"));
 app.use(require("./routes"));
 app.use(notFoundHandler);
--- app/middleware.js
+++ app/middleware.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
-const middleware = [morgan("dev"), cors(), express.json()];
+const middleware = [
+  morgan("dev"),
+  cors(),
+  express.json(),
+  rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 10,
+  }),
+];
 
 module.exports = middleware;
